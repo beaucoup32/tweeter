cfe2336fa51855a172e39c97ed71e959158bbe72
Donnell Harris <donnell.harris@koodomobile.com>
2022-12-07 16:06:36 -0800

    add timeago CDN to article template

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,33 +5,6 @@
  */
 
 $(() => {
-
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-  
   const createTweetElement = (tweetData) => {
 
     let $tweet = $(`
@@ -46,7 +19,7 @@ $(() => {
           </div>
         <div>
           <footer>
-            <p class="date">${tweetData.created_at}</p>
+            <p class="date">${timeago.format(tweetData.created_at)}</p>
             <div class="logos">
               <i class="fa-regular fa-flag fs-lg"></i>
               <i class="fa-solid fa-repeat"></i>
@@ -71,7 +44,16 @@ $(() => {
     
   }
   
-  renderTweets(data);
+  const loadtweets = () => {
+    $.getJSON('/tweets', (data) => {
+      console.log(data);
+      renderTweets(data);
+    })
+
+  }
+
+  loadtweets();
+
 
   const $form = $('.post-tweet');
 
