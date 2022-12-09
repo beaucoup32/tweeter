d2272e35204c30f813fcb3148749be44425d250a
Donnell Harris <donnell.harris@koodomobile.com>
2022-12-08 20:24:31 -0800

    add desktop + mobile layout

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,37 +1,25 @@
+$(document).ready(function () {
+  let counter = $(".counter");
+  let textBox = $("#tweet-text");
 
-
-$(document).ready(function() {
-
-  let counter = $('.counter');
-  let textBox = $('#tweet-text');
-  
-  textBox.on('input', function() {
-    
+  textBox.on("input", function () {
     let charLimit = 140;
-    let input = (this.value).length;
-    
-    charLimit -= input;
-    
-    counter.text(charLimit);
-    
-    // console.log((this.value).length);
-    
-    if (input > 140) {
-      if (!counter.hasClass('max-char-limit')){
+    let input = this.value.length;
 
-        counter.toggleClass('max-char-limit');
+    charLimit -= input;
+
+    counter.text(charLimit);
+
+    if (input > 140) {
+      if (!counter.hasClass("max-char-limit")) {
+        counter.toggleClass("max-char-limit");
       }
-    } 
+    }
 
     if (input <= 140) {
-      if (counter.hasClass('max-char-limit')){
-
-        counter.toggleClass('max-char-limit');
+      if (counter.hasClass("max-char-limit")) {
+        counter.toggleClass("max-char-limit");
       }
-    } 
-    // console.log(counter);
-  })
-
-
+    }
+  });
 });
-
